1a93758c63992e6eac0fb92d7bb974004ad8e337
kidus-t-w <kidusk69@gmail.com>
2024-04-27 16:21:51 +0300

    Fixed Task 0

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,8 +1,7 @@
 -- Creates a table users following the following requirements.
 
 CREATE TABLE IF NOT EXISTS users (
-    id INT NOT NULL,
-    email VARCHAR(225) NOT NULL,
-    name VARCHAR(225),
-    PRIMARY KEY(id)
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    email VARCHAR(225) NOT NULL UNIQUE,
+    name VARCHAR(225)
 );
