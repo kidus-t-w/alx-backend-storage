5e57e0406e5983da65a0012270c4bb199747141b
kidus-t-w <kidusk69@gmail.com>
2024-04-27 16:56:29 +0300

    Fixed task 0, 1

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,4 +1,8 @@
 -- Creates a table users following the following requirements.
+-- id, integer, never null, auto increments and primary key
+-- email, string (225 characters), never null and  unique
+-- name, string (225 characters)
+-- If table exists, script will not fail, can be executed on any database
 
 CREATE TABLE IF NOT EXISTS users (
     id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,8 +1,13 @@
--- Creates a table users
+-- Creates a table users following the following requirements.
+-- id, integer, never null, auto increments and primary key
+-- email, string (225 characters), never null and  unique
+-- name, string (225 characters)
+-- country, enum of countries: US, CO and TN, never null (default=US)
+-- If table exists, script will not fail, can be executed on any database
 
 CREATE TABLE IF NOT EXISTS users (
     id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     email VARCHAR(225) NOT NULL UNIQUE,
     name VARCHAR(225),
-    country VARCHAR ENUM('US', 'CO', 'TN') DEFAULT 'US' NEVER NULL
+    country ENUM('US', 'CO', 'TN') DEFAULT 'US' NOT NULL
 )
